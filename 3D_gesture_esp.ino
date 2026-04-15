2a54549b56cb53e9744dfe7842f8e05373fda769
Jibitesh3083 <jibitesh101@gmail.com>
2026-04-15 16:22:38 +0530

    Add files via upload

---
+++ 3D_gesture_esp.ino
@@ -0,0 +1,69 @@
+const int PUL = 3;
+const int DIR = 4;
+
+const int stepsPerRev = 3200;
+
+void setup() {
+  pinMode(PUL, OUTPUT);
+  pinMode(DIR, OUTPUT);
+
+  Serial.begin(115200);
+  Serial.println("Enter: angle direction");
+  Serial.println("Example: 90 1  (1=CW, 0=CCW)");
+}
+
+// 🔵 Position control
+void stepperPosition(float angle, int dir) {
+  digitalWrite(DIR, dir);
+
+  int steps = round((angle / 360.0) * stepsPerRev);
+
+  for(int i = 0; i < steps; i++) {
+    digitalWrite(PUL, HIGH);
+    delayMicroseconds(200);
+    digitalWrite(PUL, LOW);
+    delayMicroseconds(200);
+  }
+}
+
+// 🔵 Continuous motion (not used here but kept)
+void stepperMove(int dir) {
+  digitalWrite(DIR, dir);
+
+  digitalWrite(PUL, HIGH);
+  delayMicroseconds(22500);
+  digitalWrite(PUL, LOW);
+  delayMicroseconds(22500);
+}
+
+void loop() {
+
+  if (Serial.available()) {
+
+    String input = Serial.readStringUntil('\n');
+    input.trim();  // remove spaces + \r
+
+    int spaceIndex = input.indexOf(' ');
+
+    if (spaceIndex > 0) {
+
+      String angleStr = input.substring(0, spaceIndex);
+      String dirStr   = input.substring(spaceIndex + 1);
+
+      float angle = angleStr.toFloat();
+      int dir = dirStr.toInt();
+
+      Serial.print("Moving ");
+      Serial.print(angle);
+      Serial.print(" degrees | DIR: ");
+      Serial.println(dir);
+
+      stepperPosition(angle, dir);
+
+      Serial.println("Done\n");
+    } 
+    else {
+      Serial.println("Invalid input! Use: angle direction");
+    }
+  }
+}
